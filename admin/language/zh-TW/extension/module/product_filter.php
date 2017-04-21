17d8e1c0b3659941b404b9cf00b06ee7186f60f3
Mars-Home <bcawosxy@gmail.com>
2017-04-22 00:28:54 +0800

    v1.1.1
    1.測試直接上傳模組"進階商品管理"

---
+++ admin/language/zh-TW/extension/module/product_filter.php
@@ -0,0 +1,98 @@
+<?php
+$_['heading_title']		= '商品管理器';
+// Text
+$_['option_yes']        = '是';
+$_['option_no']         = '否';
+$_['option_enabled']    = '啟用';
+$_['option_disabled']   = '關閉';
+
+/*Setting columns*/
+$_['column_field_name']  = '欄位名稱';
+$_['column_type']        = '欄位類型';
+$_['column_value']       = '預設值';
+$_['column_options']     = '選項';
+$_['column_entry_text']  = 'Entry text';
+$_['column_rules']       = 'Rules';
+$_['column_autocomplete']= '自動完成';
+$_['column_inline_edit'] = '快速編輯';
+$_['column_enable']      = '篩選器';
+$_['column_show_column'] = '顯示欄位';
+
+/*Product table columns*/
+$_['column_name']            = '產品名稱';
+$_['column_model']           = '型號';
+$_['column_image']           = '圖片';
+$_['column_price']           = '價格';
+$_['column_quantity']        = '數量';
+$_['column_status']          = '狀態';
+$_['column_tag'] 			 = '標籤';
+$_['column_weight'] 		 = '重量';
+$_['column_shipping'] 		 = '運送';
+$_['column_minimum'] 		 = '最小數量';
+$_['column_location'] 		 = '地點';
+$_['column_date_available']  = '生效日期';
+$_['column_sku']  			 = 'SKU';
+$_['column_upc']  			 = 'UPC';
+$_['column_ean']  			 = 'EAN';
+$_['column_jan']  			 = 'JAN';
+$_['column_isbn']  			 = 'ISBN';
+$_['column_mpn']  			 = 'MPN';
+$_['column_keyword']          = 'SEO 關鍵字';
+$_['column_manufacturer_id']  = '品牌';
+$_['column_stock_status_id']  = '缺貨狀態';
+$_['column_points']           = 'Points to buy';
+$_['column_tax_class']        = '稅別';
+$_['column_height']			  = '高度';
+$_['column_weight_class_id']  = '重量類別';
+$_['column_length_class_id']  = '長度類別';
+$_['column_tax_class_id'] 	  = '稅別ID';
+$_['column_width']			  = '寬度';
+$_['column_length']			  = '長度';
+$_['column_subtract']         = 'Subtract Stock';
+$_['column_viewed']	          = 'Viewed';
+$_['column_category']	      = '類別';
+$_['column_sort_order']	      = '排序方式';
+$_['column_product_id']	      = 'ID';
+$_['column_meta_keywords']	  = 'Meta keywords';
+$_['column_meta_description'] = 'Meta description';
+$_['column_meta_title'] 	  = 'Meta title';
+
+/*Text*/
+$_['count_selected_text'] 	  = 'Shown # of % columns';
+$_['select_placeholder_text'] = '選擇可用欄位';
+$_['all_selected_text'] 	  = '顯示所有欄位';
+$_['button_filter_setting']   = '選項';
+$_['clear_all_filters_text']  = '清除條件';
+$_['show_filters_text'] 	  = '搜尋條件';
+$_['hide_filters_text'] 	  = '關閉條件';
+$_['text_not_like']			  = 'Not like';
+$_['text_like']			  	  = 'Like';
+$_['text_anywhere']		  	  = 'Anywhere';
+$_['text_nowhere']		  	  = 'Nowhere';
+$_['text_regex']		  	  = 'Regex';
+$_['text_with_image']		  = 'With image';
+$_['text_no_image']			  = 'No image';
+$_['text_save_field']		  = '儲存';
+$_['text_cancel_field']		  = '取消';
+$_['asc_text']		  		  = '低至高';
+$_['desc_text']		  	  	  = '高至低';
+$_['text_list']			  	  = '欄位';
+
+/*Error*/
+$_['text_success']			  = 'Success: Filters were successfully modified.';
+$_['text_warning']			  = 'Warning: Something happened, filters cannot be modified (contact developer)!';
+$_['error_permission']		  = 'Warning: You do not have permission to modify enabled filters!';
+$_['error_edit_field'] 		  = 'Warning: Cannot create edit field!';
+$_['error_product_permission'] = 'Warning: You do not have permission to modify product!';
+$_['error_field_name'] 		  = 'Warning: Field name must be string and cannot be empty or longer than 300 characters!';
+$_['error_content'] 		  ='Warning: Content cannot be empty or longer than 300 characters!';
+$_['error_input_name'] 		  = 'Warning: InputName must be string and cannot be empty or longer than 300 characters!';
+$_['error_product_id'] 		  = 'Warning: ProductId is not integer!';
+$_['error_update_field'] 	  = 'Warning: Cannot update product!';
+$_['error_updating_product']  = 'Warning: Error while updating product!';
+$_['error_autocomplete']  	  = 'Warning: Cannot autocomplete!';
+$_['error_change_shown_columns'] = 'Warning: Cannot change shown columns!';
+$_['error_update_filter_inputs'] = 'Warning: Cannot update filter inputs!';
+$_['error_filter_products'] = 'Warning: Cannot filter products!';
+
+?>
