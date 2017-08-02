3b4607a6fd39107f21883717eb1eddaab47520be
Mars-Dev <bcawosxy@gmail.com>
2017-08-02 16:06:31 +0800

    v1.3.0
    1.加入 ajax one page checkout 套件

---
+++ catalog/language/english/module/d_quickcheckout.php
@@ -0,0 +1,64 @@
+<?php 
+
+// ENG ver
+
+// $_['title_payment_address']  						= 'Payment details';
+// $_['description_payment_address']  					= '';
+// $_['title_shipping_address']  						= 'Delivery details';
+// $_['description_shipping_address'] 					= '';
+// $_['title_shipping_method']  						= 'Shipping method';
+// $_['description_shipping_method'] 					= '';
+// $_['title_payment_method'] 							= 'Payment method';
+// $_['description_payment_method'] 					= '';
+// $_['title_shopping_cart'] 							= 'Shopping cart';
+// $_['description_shopping_сart'] 					= '';
+
+
+// $_['placeholder_telephone'] 						= '050 123 45 54';
+
+// $_['error_field_required'] 							= 'This field is required';
+// $_['error_valid_telephone'] 						= 'Please enter a valid telephone number';
+// $_['entry_email_confirm'] 							= 'Confirm E-mail';
+// $_['error_email_confirm'] 							= 'Email confirmation does not match email!';
+
+
+// $_['step_option_guest_desciption'] = 'Use guest checkout to buy without creating an account';
+
+// $_['error_step_payment_address_fields_company'] = 'Company name required more the 3 and less the 128 characters';
+// $_['error_step_shipping_address_fields_company'] = 'Company name required more the 3 and less the 128 characters';
+
+// $_['error_step_confirm_fields_comment'] = 'Please fill in the comment to the order';
+
+
+
+// Chinese ver
+
+$_['title_payment_address']  						= '付款資訊';
+$_['description_payment_address']  					= '';
+$_['title_shipping_address']  						= '交貨資訊';
+$_['description_shipping_address'] 					= '';
+$_['title_shipping_method']  						= '運送方式';
+$_['description_shipping_method'] 					= '';
+$_['title_payment_method'] 							= '付款方式';
+$_['description_payment_method'] 					= '';
+$_['title_shopping_cart'] 							= '購物車';
+$_['description_shopping_сart'] 					= '';
+
+
+// $_['placeholder_telephone'] 						= '050 123 45 54';
+
+$_['error_field_required'] 							= '必須同意才可進行結帳';
+$_['error_valid_telephone'] 						= '請輸入合法的電話號碼';
+$_['entry_email_confirm'] 							= '信箱確認';
+$_['error_email_confirm'] 							= '電子信箱不相符';
+
+
+$_['step_option_guest_desciption'] = 'Use guest checkout to buy without creating an account';
+
+$_['error_step_payment_address_fields_company'] = '名稱至少需要輸入三個字元';
+$_['error_step_shipping_address_fields_company'] = '名稱至少需要輸入三個字元';
+
+$_['error_step_confirm_fields_comment'] = 'Please fill in the comment to the order';
+
+
+?>
