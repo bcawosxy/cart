9ed30f50faa6e1494036fe045e1c77e7cc04e3fb
Mars-Dev <bcawosxy@gmail.com>
2017-10-25 17:13:01 +0800

    v1.7.2
    1.修改後台底部文字

--- admin/language/zh-TW/common/footer.php
+++ admin/language/zh-TW/common/footer.php
@@ -4,5 +4,5 @@
 // $_['text_version'] 	= '版本 %s';
 
 // Text
-$_['text_footer'] 	= ' <a href="http://www.hao-young.com">好漾市集</a> &copy; ' . date('Y') . ' All Rights Reserved.';
+$_['text_footer'] 	= ' <a href="http://www.hao-young.com">昊洋國際有限公司</a> &copy; ' . date('Y') . ' All Rights Reserved.';
 $_['text_version'] 	= '';
