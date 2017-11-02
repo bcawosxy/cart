1f54c4c321574d9594679c63720bab3c8a2428b5
Mars-Dev <bcawosxy@gmail.com>
2017-11-02 14:41:19 +0800

    v1.8.1
    1.修正語法錯誤

--- catalog/model/account/wishlist.php
+++ catalog/model/account/wishlist.php
@@ -3,7 +3,7 @@ class ModelAccountWishlist extends Model {
 	public function addWishlist($product_id) {
 		$this->db->query("DELETE FROM " . DB_PREFIX . "customer_wishlist WHERE customer_id = '" . (int)$this->customer->getId() . "' AND product_id = '" . (int)$product_id . "'");
 
-		$this->db->query("INSERT INTO " . DB_PREFIX . "customer_wishlist SET customer_id = '" . (int)$this->customer->getId() . "', product_id = '" . (int)$product_id . "', date_added = ''".INSERTTIME."'");
+		$this->db->query("INSERT INTO " . DB_PREFIX . "customer_wishlist SET customer_id = '" . (int)$this->customer->getId() . "', product_id = '" . (int)$product_id . "', date_added = '".INSERTTIME."'");
 	}
 
 	public function deleteWishlist($product_id) {
